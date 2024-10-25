211e9be1845df3fcdb2dfc26aa7d80229a534248
bielmc11 <bielmarticr@gmail.com>
2024-10-25 20:33:18 +0200

    Rols sesions

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,19 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import prisma from "./lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub, Google],
-
+  callbacks: {
+    async session({ session, token, user }) {
+      //Aqui tengo que retornar la session modificada con el rol:
+      //Dentro del token o de la sesion.user puedo encontrar el correo
+      //con el correo encontrado hago un findUnique a primsa.user
+      //Añado el resultado a session.user.roles
+      return session;
+    },
+  },
 });
- 
