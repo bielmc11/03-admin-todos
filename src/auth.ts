2bffb143cd5e2285cca7ddb44b80ceb81278c041
bielmc11 <bielmarticr@gmail.com>
2024-10-27 13:07:42 +0100

    type Session user

--- src/auth.ts
+++ src/auth.ts
@@ -1,18 +1,42 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import prisma from "./lib/prisma";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      roles: string[];
+    } & DefaultSession["user"];
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [GitHub, Google],
+  session: {
+    strategy: "jwt",
+  },
   callbacks: {
+    async jwt({ token, user, account, profile }) {
+      const dbUser = await prisma.user.findUnique({
+        where: {
+          email: token.email ?? "",
+        },
+      });
+      token.roles = dbUser?.roles ?? ["no-roles"];
+      token.id = dbUser?.id ?? "no-uuid";
+
+      return token;
+    },
+
     async session({ session, token, user }) {
-      //Aqui tengo que retornar la session modificada con el rol:
-      //Dentro del token o de la sesion.user puedo encontrar el correo
-      //con el correo encontrado hago un findUnique a primsa.user
-      //Añado el resultado a session.user.roles
+      if (session && session.user) {
+        session.user.roles = token.roles as string[];
+        session.user.id = token.id as string;
+      }
+      console.log("My sesion es session", session);
       return session;
     },
   },
